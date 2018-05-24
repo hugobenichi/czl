7a904a16c0f82b41ee126648a01520383cbb4f83
Hugo Benichi <hugobenichi@gmail.com>
2018-05-24 23:54:09 +0000

    config: cpp-ification !

--- config.c
+++ config.c
@@ -29,6 +29,8 @@ typedef uint32_t  u32;
 typedef uint64_t  u64;
 
 
+#define _arraylen(ary)    (sizeof(ary)/sizeof(ary[0]))
+
 #define _stringize2(x) #x
 #define _stringize(x) _stringize2(x)
 #define _source_loc() __FILE__ ":" _stringize(__LINE__)
@@ -52,98 +54,103 @@ void fail_at_location_if(int has_failed, const char *loc, const char * msg, ...)
         exit(1);
 }
 
+typedef int (*char_filter)(u8);
+
 struct slice {
 	u8* start;
 	u8* stop;
+
+
+	int writ_to(int fd)
+	{
+		return write(fd, (void*) start, (size_t)(stop - start));
+	}
+
+	i32 len()
+	{
+		return stop - start;
+	}
+
+	i32 is_empty()
+	{
+		return stop == start;
+	}
+
+	char* str()
+	{
+		int l = len();
+		char* str = (char*) malloc(l + 1);
+		memcpy(str, start, l);
+		str[l] = 0;
+		return str;
+	}
+
+	slice drop(i32 n)
+	{
+		slice s = *this;
+		n = _min(n, s.len());
+		s.start += n;
+		return s;
+	}
+
+	slice split(int c)
+	{
+		slice left = *this;
+		u8* pivot = (u8*) memchr(left.start, c, left.len());
+		if (!pivot) {
+			pivot = stop;
+		}
+
+		left.stop = pivot;
+		start = pivot;
+
+		return left;
+	}
+
+	slice strip(u8 c)
+	{
+		slice s = *this;
+		while (s.len() && *(s.stop - 1) == c) {
+			s.stop--;
+		}
+		return s;
+	}
+
+	slice take_line()
+	{
+		slice line = split('\n');
+		if (*start == '\n') {
+			start++;
+		}
+		return line;
+	}
+
+	slice slice_while(char_filter fn, int inverse)
+	{
+		u8* u = start;
+		while (u < stop && inverse ^ fn(*u)) {
+			u++;
+		}
+		slice left = *this;
+		left.stop = u;
+		start = u;
+		return left;
+
+	}
+
+	slice take_while(char_filter fn)
+	{
+		return slice_while(fn, 0);
+	}
+
+	slice drop_until(char_filter fn)
+	{
+		return slice_while(fn, 1);
+	}
+
 };
 
-static const struct slice slice_empty = { .start = 0, .stop = 0 };
-
-int slice_write(int fd, struct slice s)
-{
-	return write(fd, (void*) s.start, (size_t)(s.stop - s.start));
-}
-
-i32 slice_len(struct slice s)
-{
-	return s.stop - s.start;
-}
-
-i32 slice_is_empty(struct slice s)
-{
-	return s.stop == s.start;
-}
-
-char* slice_to_string(struct slice s)
-{
-	int len = slice_len(s);
-	char* str = malloc(len + 1);
-	memcpy(str, s.start, len);
-	str[len] = 0;
-	return str;
-}
-
-struct slice slice_drop(struct slice s, i32 n)
-{
-	n = _min(n, slice_len(s));
-	s.start += n;
-	return s;
-}
-
-struct slice slice_split(struct slice *s, int c)
-{
-	struct slice left = *s;
-	u8* pivot = memchr(left.start, c, slice_len(left));
-	if (!pivot) {
-		pivot = s->stop;
-	}
-
-	left.stop = pivot;
-	s->start = pivot;
-
-	return left;
-}
-
-struct slice slice_strip(struct slice s, u8 c)
-{
-	while (slice_len(s) && *(s.stop-1) == c) {
-		s.stop--;
-	}
-	return s;
-}
-
-struct slice slice_take_line(struct slice *s)
-{
-	struct slice line = slice_split(s, '\n');
-	if (*s->start == '\n') {
-		s->start++;
-	}
-	return line;
-}
-
-typedef int (*char_filter)(u8);
-
-struct slice slice_while(struct slice *s, char_filter fn, int inverse)
-{
-	u8* u = s->start;
-	while (u < s->stop && inverse ^ fn(*u)) {
-		u++;
-	}
-	struct slice left = { .start = s->start, .stop = u };
-	s->start = u;
-	return left;
-
-}
-
-struct slice slice_take_while(struct slice *s, char_filter fn)
-{
-	return slice_while(s, fn, 0);
-}
-
-struct slice slice_drop_until(struct slice *s, char_filter fn)
-{
-	return slice_while(s, fn, 1);
-}
+static const slice slice_empty = { .start = 0, .stop = 0 };
 
 int char_is_space(u8 c) {
 	return c == ' ' || c == '\t';
@@ -154,17 +161,17 @@ struct keyval {
 	struct slice val;
 };
 
-struct keyval keyval_from_line(struct slice s)
+struct keyval keyval_from_line(slice s)
 {
 	struct keyval kv;
 
-	s = slice_split(&s, '#');
+	s = s.split('#');
 
-	slice_take_while(&s, char_is_space);
-	kv.key = slice_drop_until(&s, char_is_space);
+	s.take_while(char_is_space);
+	kv.key = s.drop_until(char_is_space);
 
-	slice_take_while(&s, char_is_space);
-	kv.val = slice_drop_until(&s, char_is_space);
+	s.take_while(char_is_space);
+	kv.val = s.drop_until(char_is_space);
 
 	return kv;
 }
@@ -175,8 +182,10 @@ struct mapped_file {
 	struct slice data;
 };
 
-int mapped_file_load(struct mapped_file *f)
+int mapped_file_load(struct mapped_file *f, const char* path)
 {
+	memcpy(&f->name, path, _arraylen(f->name));
+
 	int fd = open(f->name, O_RDONLY);
 	_fail_if(fd < 0, "open %s failed: %s", f->name, strerror(errno));
 
@@ -188,7 +197,7 @@ int mapped_file_load(struct mapped_file *f)
 	int offset = 0;
 
 	size_t len = f->file_stat.st_size;
-	f->data.start = mmap(NULL, len, prot, flags, fd, offset);
+	f->data.start = (u8*) mmap(NULL, len, prot, flags, fd, offset);
 	f->data.stop  = f->data.start + len;
 	_fail_if(f->data.start == MAP_FAILED, "mapped_file_load(%s) failed: %s", f->name, strerror(errno));
 
@@ -203,27 +212,24 @@ struct configuration {
 
 int main(int argc, char** args)
 {
-	struct mapped_file config = {
-		.name = "./config.txt"
-	};
-	mapped_file_load(&config);
-	int n = 0;
+	struct mapped_file config;
+	mapped_file_load(&config, "./config.txt");
 
-	while (slice_len(config.data)) {
-		struct slice line = slice_take_line(&config.data);
-		if (slice_is_empty(line)) {
+	while (config.data.len()) {
+		slice line = config.data.take_line();
+		if (line.is_empty()) {
 			continue;
 		}
 
 		struct keyval kv = keyval_from_line(line);
-		if (slice_is_empty(kv.key)) {
+		if (kv.key.is_empty()) {
 			continue;
 		}
 
-		if (slice_is_empty(kv.val)) {
-			printf("W: no val for key '%s'\n", slice_to_string(kv.key));
+		if (kv.val.len()) {
+			printf("key:%s val:%s\n", kv.key.str(), kv.val.str());
 		} else {
-			printf("key:%s val:%s\n", slice_to_string(kv.key), slice_to_string(kv.val));
+			printf("W: no val for key '%s'\n", kv.key.str());
 		}
 	}
 }
